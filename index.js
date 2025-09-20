31999edf66031f5aced00e0d62f687cc51aaaa46
+ <+>
2025-09-20 12:54:47 +0300

    solution

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const main = document.querySelector("#main");
+if (main) {
+  main.remove();
+}
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "wachira is the champion";
+document.body.append(newHeader);
